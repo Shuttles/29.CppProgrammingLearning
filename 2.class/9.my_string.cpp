d5cb1c0b9281e48072df7df53f6daddf33cd990e
chenzheyu <2287441467@qq.com>
2020-07-30 23:23:02 +0800

    some changes

---
+++ 2.class/9.my_string.cpp
@@ -0,0 +1,40 @@
+/*************************************************************************
+	> File Name: 9.my_string.cpp
+	> Author: 少年宇
+	> Mail: 
+	> Created Time: 2020年07月30日 星期四 12时22分11秒
+ ************************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <vector>
+using namespace std;
+
+namespace haizei {
+
+class string {
+
+
+};
+
+
+}
+
+
+
+
+int main() {
+    haizei::string str1;
+    cin >> str1;
+    cout << str1;
+
+    return 0;
+}
