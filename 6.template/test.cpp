ced693b06297ff36ebb4bc95fb7f4837306b381f
chenbin <2287441467@qq.com>
2020-08-10 22:51:35 +0800

    notes

---
+++ 6.template/test.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 一  8/10 22:43:48 2020
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+//所以说右值可以绑定在左值引用上！只不过左值引用得写成const int &
+const int &add(int &&a, int &&b) {
+    return a + b;
+}
+
+
+int main() {
+    cout << add(2, 4) << endl;
+    return 0;
+}
